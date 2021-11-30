ffaa3e55bf75036214c33b76a473bcafa2457017
wilson <wilson.silva@tegra.com.br>
2021-11-30 09:33:16 -0300

    fix new Date

--- src/job/module/job-start.ts
+++ src/job/module/job-start.ts
@@ -2,11 +2,6 @@ import { JobWork } from "../index";
 import { getJob } from "../service/api.axios";
 
 export class JobStart {
-  date: string;
-
-  constructor() {
-    this.date = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
-  }
   start(config): void {
     JobWork.start({
       config: config,
@@ -14,8 +9,14 @@ export class JobStart {
     });
   }
 
+  private getDate() {
+    return new Date().toLocaleString("pt-BR", {
+      timeZone: "America/Sao_Paulo",
+    });
+  }
+
   async execute(): Promise<void> {
-    const data = await getJob('https://job-peter-drucker.herokuapp.com/')
-    console.log(`${data}: ${this.date}`);
+    const data = await getJob("https://job-peter-drucker.herokuapp.com/");
+    console.log(`${data}: ${this.getDate()}`);
   }
 }
