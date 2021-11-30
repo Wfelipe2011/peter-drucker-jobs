a8b3428e8187edce7af061d2b20dcb99b931561d
wilson <wilson.silva@tegra.com.br>
2021-11-30 06:31:27 -0300

    adicionado get

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,31 @@
 import express from "express";
-import dotenv from 'dotenv'
-import { JobEmail } from './job/module/job-email';
-import { JobGet } from './job/module/job-get-user-schedule';
-import { JobStart } from './job/module/job-start';
+import dotenv from "dotenv";
+import { JobEmail } from "./job/module/job-email";
+import { JobGet } from "./job/module/job-get-user-schedule";
+import { JobStart } from "./job/module/job-start";
 
 dotenv.config();
 const app = express();
 // teste por minuto '*/1 * * * *'
 // jobEmailInfo.start({ hour: 8, minute: 59, dayOfWeek: [1, 2, 3, 4, 5], tz: 'Etc/GMT-3' });
 // jobUserSchedule.start({ hour: 9, minute: 20 });
-const jobInfo = new JobEmail()
-jobInfo.start({ hour: 8, minute: 59, dayOfWeek: [1, 2, 3, 4, 5], tz: 'Etc/GMT-3' })
+const jobInfo = new JobEmail();
+jobInfo.start({
+  hour: 8,
+  minute: 59,
+  dayOfWeek: [1, 2, 3, 4, 5],
+  tz: "Etc/GMT-3",
+});
 
-const jobGet = new JobGet()
-jobGet.start('*/10 * * * *')
+const jobGet = new JobGet();
+jobGet.start("*/10 * * * *");
 
-const jobStart = new JobStart()
-jobStart.start('*/1 * * * *')
+const jobStart = new JobStart();
+jobStart.start("*/1 * * * *");
+
+app.get("/", (req, res) => {
+  res.send("Estou vivo").json()
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`> Sever listening on port: ${process.env.PORT}`);
