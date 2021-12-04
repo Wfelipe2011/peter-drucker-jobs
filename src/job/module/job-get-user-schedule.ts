2d480ef64581310a6d8467b5dc51933e6a905530
wilson <wilson.silva@tegra.com.br>
2021-12-04 15:53:36 -0300

    teste mensagem whats

--- src/job/module/job-get-user-schedule.ts
+++ src/job/module/job-get-user-schedule.ts
@@ -1,6 +1,21 @@
 
 import { JobWork } from '../index';
 import { get } from '../service/api.axios';
+import wbm from 'wbm';
+
+// Modulo WhatsApp
+
+function WhatsOneMessage(numero, text) {
+
+    return wbm.start().then(async () => {
+        const phones = [numero]
+        const message = text;
+        await wbm.send(phones, message);
+        await wbm.end();
+    }).catch(err => console.log(err));
+}
+
+
 
 export class JobGet {
   start(config): void {
@@ -9,8 +24,11 @@ export class JobGet {
       start: this.execute.bind(this),
     });
   }
-
+  
   async execute(): Promise<void> {
+    WhatsOneMessage(5515981428548, `Oi gatinha ${new Date().toLocaleString("pt-BR", {
+      timeZone: "America/Sao_Paulo",
+    })}`)
     await get('user_schedule/job') 
   }
 }
